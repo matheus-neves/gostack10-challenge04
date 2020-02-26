73cf1b78516784f1a56a0ef90f8a3bb769d23adf
Matheus Neves <matheus.neves@prodigious.com>
2020-02-26 15:51:06 -0300

    start challenge, webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { resolve } = require('path');
+
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: resolve(__dirname, 'public'),
+    port: 3000
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: 'file-loader',
+        }
+      }
+  
+    ]
+  }
+  
+}
